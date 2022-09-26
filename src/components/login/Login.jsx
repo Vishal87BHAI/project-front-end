73ac3f90a4d77537c98a209cfb5371db9477c4ad
Vishal87BHAI <vishal878787k@gmail.com>
2022-09-26 15:57:50 +0530

    try

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,22 @@
+import { NavLink } from "react-router-dom";
 
-
-const Login=()=>{
-    return(
-        <div>
-            <h1 style={{marginTop: "50px"}}>Login page</h1>
+const Login = () => {
+    return (
+        <div style={{ paddingTop: "100px" }}>
+            <h1>Login</h1><br/>
+                <form style={{ width: "900px",paddingLeft:"470px" }}>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div><br/>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div><br/>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form><br/>
+                <span><NavLink to={"/Register"}>Not a user Register here</NavLink></span>
         </div>
     )
 }
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,9 +1,38 @@
 
 
-const Register=()=>{
-    return(
+const Register = () => {
+    return (
         <div>
-            <h1 style={{marginTop: "50px"}}>Register page</h1>
+            <h1 style={{ marginTop: "70px" }}>Register page</h1><br />
+            <form style={{ paddingLeft: "470px" }}>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="Name">Name</label>
+                        <input type="text" class="form-control" id="Name" placeholder="Enter your name" />
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="DOB">Name</label>
+                        <input type="text" class="form-control" id="DOB" placeholder="Date of birth" />
+                    </div>
+                    <div class="form-check col-md-6" ><br />
+                        <label for="Gender">Gender</label><br />
+                        <input class="form-check-input" type="radio" name="Gender" id="Male" /><label class="form-check-label" for="Male">Male</label><br/>
+                        <input class="form-check-input" type="radio" name="Gender" id="Female" /><label class="form-check-label" for="Female">Female</label><br/>
+                        <input class="form-check-input" type="radio" name="Gender" id="Others" /><label class="form-check-label" for="Others">Others</label><br/>
+                    </div><br/>
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+                    </div>
+                </div><br />
+                <div class="form-group col-md-6">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </form>
         </div>
     )
 }
